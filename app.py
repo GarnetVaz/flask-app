87acd780df25744a5754135c357ea0acb1ac0d2d
Garnet Vaz <garnet.vaz@gmail.com>
2016-10-20 22:10:56 -0700

    more routes

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def hello_world():
     return "Hello world!"
 
+
+@app.route('/info/<username>')
+def test_list(username):
+    return 'User %s' % username
+
+
+@app.route('/projects/')
+def projects():
+    return "Project list"
+
+
+@app.route('/about')
+def about():
+    return "The about page"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
