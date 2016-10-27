64cdeb39c099b8bbfeeb06db64ac2ca02ff3a79c
Garnet Vaz <garnet.vaz@gmail.com>
2016-10-26 23:17:57 -0700

    latest

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-from flask import Flask
 from flask import render_template
+from flask import g
 from flask import request
 from flask import abort
 from flask import redirect, url_for
-from flask import g
 from flask.json import jsonify
 import psycopg2 as pg
+from flask import Flask
+from flask.ext.cache import Cache
+import requests
+import configparser
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+config = configparser.ConfigParser()
+_ = config.read('config.ini')
+
 app.config.update(dict(
-    DATABASE='Website',
-    SECRET_KEY='secretkey',
-    USERNAME='Garnet',
-    PASSWORD='edna1986'
+    DATABASE=config.get('Database', 'database'),
+    SECRET_KEY=config.get('Application', 'secretkey'),
+    USERNAME=config.get('Database', 'username'),
+    PASSWORD=config.get('Database', 'password'),
+    HOST=config.get('Database', 'host'),
+    OPENWEATHERMAPKEY=config.get('ApiKeys', 'openweathermap')
 ))
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 def connect_db():
@@ -24,7 +34,7 @@ def connect_db():
         database=app.config['DATABASE'],
         user=app.config['USERNAME'],
         password=app.config['PASSWORD'],
-        host='localhost')
+        host=app.config['HOST'])
     return conn
 
 
@@ -67,17 +77,18 @@ def add_db_user(name, password):
     conn = get_db()
     cur = conn.cursor()
     try:
-        cur.execute('insert into users (username, password) values (%s, %s)', name, password)
-    except:
-        pass
+        cur.execute('insert into users (username, password) values (%s, %s)', (name, password))
+    except Exception as e:
+        print(e)
     cur.close()
     conn.commit()
 
 
 @app.route('/db')
-def db(name=None):
+def db():
     res = run_db_user_search()
     return jsonify(res)
+
 
 @app.route('/')
 def hello_world():
@@ -88,16 +99,31 @@ def hello_world():
 def signin():
     return render_template('login.html')
 
+
 @app.route('/signup')
 def signup():
-    return render_template('signup')
+    return render_template('signup.html')
+
+
+@app.route('/weather')
+@cache.cached(timeout=60*10)
+def get_weather():
+    url = 'http://api.openweathermap.org/data/2.5/forecast/city?id=5786882&APPID=' + \
+          app.config['OPENWEATHERMAPKEY']
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.text
+    else:
+        return "API call failed."
+
 
 @app.route('/signupuser', methods=["POST"])
 def signupuser():
     name = request.form['username']
     password = request.form['password']
     add_db_user(name, password)
-    redirect(url_for(db))
+    return redirect(url_for('db'))
+
 
 @app.route('/login', methods=["POST"])
 def login():
